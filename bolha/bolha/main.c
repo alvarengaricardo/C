36d820466f7fde0dea5d681a6dfb8e845b528eda
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-22 03:50:54 -0300

    adição de contador

--- bolha/bolha/main.c
+++ bolha/bolha/main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 void bubbleSort(int array[], int tamanho) {
-    int i, j;
+    int i, j, contador;
+    contador = 0;
     for (i = 0; i < tamanho - 1; i++) {
         for (j = 0; j < tamanho - i - 1; j++) {
-            // Compara elementos adjacentes e os troca, se necessário
+            // Compara elementos adjacentes e os troca, se necessï¿½rio
             if (array[j] > array[j + 1]) {
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
             }
+            contador ++;
         }
     }
+    printf("%d\n", contador);
 }
 
 void imprimirArray(int array[], int tamanho) {
@@ -23,15 +26,15 @@ void imprimirArray(int array[], int tamanho) {
 }
 
 int main() {
-    int array[] = {64, 34, 25, 12, 22, 11, 90, 2, 45, 42};
+    int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     int tamanho = sizeof(array) / sizeof(array[0]);
 
-    printf("Array antes da ordenação: ");
+    printf("Array antes da ordenacao: ");
     imprimirArray(array, tamanho);
 
     bubbleSort(array, tamanho);
 
-    printf("Array depois da ordenação: ");
+    printf("Array depois da ordenacao: ");
     imprimirArray(array, tamanho);
 
     return 0;
