29931f83ccb2f383fadbb0ba30cdb79ab19088b4
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-23 23:28:10 -0300

    Insertion Sort

---
+++ insertion_sort/insertion_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void insertion_sort(int *array, int n) {
+  int i, j, key;
+
+  for (i = 1; i < n; i++) {
+    key = array[i];
+    j = i - 1;
+
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = key;
+  }
+}
+
+int main() {
+  int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+  
+  int n = sizeof(array) / sizeof(array[0]);
+
+  for (int i = 0; i < n; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n ");
+
+  insertion_sort(array, n);
+
+  for (int i = 0; i < n; i++) {
+    printf("%d ", array[i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
